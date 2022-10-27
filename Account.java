b49aa1b95bf770cc23894912db9b678aacf42b38
Muhammad Raihan Izharul Haq <94726321+raihanizharul@users.noreply.github.com>
2022-10-27 17:16:25 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,65 @@
+package Nusput.Praktikum;
+
+public class Account {
+    protected double balance;
+    
+    public Account(double initBalance){
+        balance = initBalance;
+    }
+    
+    public double getBalance(){
+        return this.balance;
+    }
+    
+    public boolean deposit (double amt){
+        balance += amt;
+        return true;
+    }
+    
+    public boolean withdraw(double amt){
+        balance -= amt;
+        return true;
+    }
+}
+
+class SavingAccount extends Account{
+    private double interestRate;
+    
+    public SavingAccount(double balance, double interest_rate){
+        super(balance);
+        interestRate = interest_rate;
+    }
+}
+
+class CheckingAccount extends Account{
+    private double overdraftProtection;
+    
+    public CheckingAccount(double balance,double protect){
+        super(balance);
+        overdraftProtection= protect;
+    }
+    
+    public CheckingAccount(double balance){
+        super(balance);
+        overdraftProtection=-1.0;
+    }
+    
+    @Override
+    public boolean withdraw(double amt){
+        double overdraftNeeded = amt - this.balance;
+        if (balance - amt >= 0.0) {
+            this.balance -= amt;
+            return true;
+        }else{
+            if (overdraftProtection != -1.0 || overdraftProtection < overdraftNeeded){
+                System.out.println("Transaksi dibatalkan");
+                return false;
+            }else{
+                this.balance = 0.0;
+                overdraftProtection -= overdraftNeeded;
+                System.out.println("Pengambilan berhasil");
+                return true;
+            }
+        }
+    }
+}
